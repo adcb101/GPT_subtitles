d8d66ce415a40438797ecf370f7c3c99a9029926
adcb101 <110447356+adcb101@users.noreply.github.com>
2023-10-22 00:13:49 +0800

    Update translate_gpt.py

--- translate_gpt.py
+++ translate_gpt.py
@@ -643,7 +643,6 @@ Guidelines:
 def translate_with_gpt(input_file, target_language='zh', source_language='en', batch_size=40, model='gpt-3.5-turbo-16k', video_info=None, no_translation_mapping=False, load_from_tmp=False):
     # Extract the file name without the extension
     str_list = [
-               '/content/2/1.4 - Implied Volatility In Depth Discussion.srt',
                '/content/2/1.5 - Understanding the Greeks.srt',
                '/content/2/1.6 - What is Delta.srt',
                '/content/2/1.7 - What is Gamma.srt',
